2bee19b0a241b3812461feffde4530ba76fa4d42
Your Name <you@example.com>
2023-01-05 01:57:41 +0000

    remove old bits to interface with partychain/adams

--- packages/react-app/src/views/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home";
-export { default as ExampleUI } from "./ExampleUI";
-export { default as Hints } from "./Hints";
-export { default as Subgraph } from "./Subgraph";
-export { default as TeaParty } from "./TeaParty";
