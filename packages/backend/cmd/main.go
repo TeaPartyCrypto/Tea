b4a95c3ceb4f1398c7567a92817a347b97d6e367
Jeff <60796676+JeffNeff@users.noreply.github.com>
2023-03-01 11:59:41 -0700

    prod cut.

--- packages/backend/cmd/main.go
+++ packages/backend/cmd/main.go
@@ -81,7 +81,7 @@ func main() {
 	c.SAASAddress = os.Getenv("SAAS_ADDRESS")
 
 	if c.SAASAddress == "" {
-		c.SAASAddress = "http://34.172.60.163:8080"
+		c.SAASAddress = "http://34.172.51.13:8080"
 	}
 
 	// create a new sugard logger
